d500b21b816b05594810dde59e59892592f4cdc1
puddings233 <puddings233@outlook.com>
2024-10-06 16:42:45 +0800

    feat: nvim asyncrun

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,21 +1,21 @@
 local mappings = {
 
 	-- disable arrow keys
-	{ key = '<up>',	func = '',	mod = { 'n', 'i' } },
+	{ key = '<up>',			func = '',	mod = { 'n', 'i' } },
 	{ key = '<right>',	func = '',	mod = { 'n', 'i' } },
-	{ key = '<down>',	func = '',	mod = { 'n', 'i' } },
-	{ key = '<left>',	func = '',	mod = { 'n', 'i' } },
+	{ key = '<down>',		func = '',	mod = { 'n', 'i' } },
+	{ key = '<left>',		func = '',	mod = { 'n', 'i' } },
 	{ key = '<right>',	func = '',	mod = { 'n', 'i' } },
 
 	-- remap movements
-	{ key = 'u',	func = 'k',		mod = { 'n', 'v' } },
-	{ key = 'k',	func = 'l',		mod = { 'n', 'v' } },
-	{ key = 'l',	func = '',		mod = { 'n', 'v' } },
-	{ key = 'LL',	func = 'u',		mod = 'n' },
-	{ key = 'U',	func = '5k',		mod = { 'n', 'v' } },
-	{ key = 'H',	func = '5h',		mod = { 'n', 'v' } },
-	{ key = 'J',	func = '5j',		mod = { 'n', 'v' } },
-	{ key = 'K',	func = '5l',		mod = { 'n', 'v' } },
+	{ key = 'u',	func = 'k',				mod = { 'n', 'v' } },
+	{ key = 'k',	func = 'l',				mod = { 'n', 'v' } },
+	{ key = 'l',	func = '',				mod = { 'n', 'v' } },
+	{ key = 'LL',	func = 'u',				mod = 'n' },
+	{ key = 'U',	func = '5k',			mod = { 'n', 'v' } },
+	{ key = 'H',	func = '5h',			mod = { 'n', 'v' } },
+	{ key = 'J',	func = '5j',			mod = { 'n', 'v' } },
+	{ key = 'K',	func = '5l',			mod = { 'n', 'v' } },
 	{ key = 'S',	func = ':w<CR>',	mod = 'n' },
 
 	-- tab control
@@ -23,12 +23,11 @@ local mappings = {
 	{ key = "tk",	func = ':+tabnext<CR>',	mod = 'n' },
 
 	-- quit
-	{ key = 'Q', func = ':lua quit_or_delete_buffer()<CR>', mod = 'n' },
+	{ key = 'Q',	func = ':lua quit_or_delete_buffer()<CR>',	mod = 'n' },
 
 	-- plugins
-	{ key = 'MDP',		func = '<Plug>MarkdownPreview',						mod = 'n' },
-	{ key = 'COQ',		func = ':COQnow<CR>',							mod = 'n' },
-	{ key = '<C-f>',	func = ':lua require"joshuto".joshuto({ edit_in_tab = true })<CR>', mod = 'n' },
+	{ key =	'RR',			func = ':AsyncRun ',	mod = 'n' },
+	{ key =	'<C-f>',	func = ':lua require"joshuto".joshuto({ edit_in_tab = true })<CR>',	mod = 'n' },
 
 };
 
---
+++ nvim/lua/plugins/asyncrun.lua
@@ -0,0 +1,6 @@
+return {
+	"skywind3000/asyncrun.vim",
+	config = function()
+		vim.g.asyncrun_open = 6
+	end
+}
