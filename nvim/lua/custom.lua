a476e1e12473dc9d6b6698a8c0a4c5153acaa835
puddings233 <puddings233@outlook.com>
2025-02-14 14:23:11 +0800

    feat(nvim): Simplify quit_or_delete_buffer()

--- nvim/lua/custom.lua
+++ nvim/lua/custom.lua
@@ -1,15 +1,7 @@
 -- this is for close tab and neovim via "Q"
 function quit_or_delete_buffer()
-	local function quit()
-		if vim.fn.tabpagenr('$') == 1 then
-			vim.cmd("q")  -- close neovim
-		else
-			vim.cmd("bdelete") -- close current tab via delete buffer
-		end
-	end
-
 	if vim.fn.mode() ~= "i" then
-		quit()
+		vim.cmd(vim.fn.tabpagenr("$") == 1 and "q" or "bdelete")
 	end
 end
 
